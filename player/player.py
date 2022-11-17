a7f58344098e651325823c9c2bcff05649879355
matt2905 <matthias2905@hotmail.fr>
2022-11-17 17:52:13 +0100

    save to continue at arrinfo

---
+++ player/player.py
@@ -0,0 +1,67 @@
+import argparse
+from typing import NoReturn
+
+from network.client import Client
+from player_logic.next_actions import get_next_actions
+
+
+class PlayerGameClient(Client):
+    def __init__(
+        self: "PlayerGameClient", server_addr: str, port: int, username: str
+    ) -> None:
+        super().__init__(server_addr, port, username, spectator=False)
+        self._commands: list[str] = []
+
+    def run(self: "PlayerGameClient") -> NoReturn:
+        while True:
+            game_data = self.read_json()
+            for farm in game_data["farms"]:
+                if farm["name"] == self.username:
+                    my_farm = farm
+                    break
+            else:
+                raise ValueError(f"My farm is not found ({self.username})")
+            print(my_farm)
+
+            commands = get_next_actions(game_data["day", my_farm])
+            for command in commands:
+                self.add_command(command)
+
+            self.send_commands()
+
+    def add_command(self: "PlayerGameClient", command: str) -> None:
+        self._commands.append(command)
+
+    def send_commands(self: "PlayerGameClient") -> None:
+        data = {"commands": self._commands}
+        print("sending", data)
+        self.send_json(data)
+        self._commands.clear()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Game client.")
+    parser.add_argument(
+        "-a",
+        "--address",
+        type=str,
+        help="name of server on the network",
+        default="localhost",
+    )
+    parser.add_argument(
+        "-p",
+        "--port",
+        type=int,
+        help="location where server listens",
+        default=1271,
+    )
+    parser.add_argument(
+        "-u",
+        "--username",
+        type=str,
+        help="name of the user",
+        default="unknown"
+    )
+    args = parser.parse_args()
+
+    client = PlayerGameClient(args.address, args.port, args.username).run()
---
+++ player/player_logic/next_actions.py
@@ -0,0 +1,4 @@
+from typing import List, Dict
+
+def get_next_actions(day: int, myfarm: Dict[str, any]) -> List[str]:
+    ...
