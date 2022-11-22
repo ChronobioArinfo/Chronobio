6c839beb8f958f931a010ce4e27639df76aa4f50
matt2905 <matthias2905@hotmail.fr>
2022-11-22 23:43:29 +0100

    start of farm class

---
+++ tests/test_player_logic/test_farm.py
@@ -0,0 +1,28 @@
+import pytest
+from player.player_logic.farm import Farm
+
+@pytest.mark.parametrize(
+    "number, expected", [
+        (2, 2),
+        (5, 5)
+    ]
+)
+def test_add_field(number, expected):
+    farm: Farm = Farm()
+
+    for _ in range(number):
+        farm.add_field()
+    assert len(farm._fields) == expected
+
+@pytest.mark.parametrize(
+    "number, expected", [
+        (2, 2),
+        (5, 5)
+    ]
+)
+def test_add_employee(number, expected):
+    farm: Farm = Farm()
+
+    for _ in range(number):
+        farm.add_employee()
+    assert len(farm._employee) == expected
