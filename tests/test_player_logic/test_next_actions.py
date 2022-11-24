0ed494c7dcab1b69caea9f5266254538b301c5d9
JosephEAK <josephakoubia@gmail.com>
2022-11-24 12:20:35 +0100

    modification test_next_action

--- tests/test_player_logic/test_next_actions.py
+++ tests/test_player_logic/test_next_actions.py
@@ -4,6 +4,7 @@ from player.player_logic.employee import Employee
 from player.player_logic.field import Field
 from player.player_logic.next_actions import day_0, init_day, give_job_employee
 from player.player_logic.state import State
+from player.player_logic.my_type import Vegetable
 
 
 def test_day_0():
@@ -19,17 +20,11 @@ def test_day_0():
         "0 EMPLOYER",
         "0 EMPLOYER",
         "0 EMPLOYER",
-        "0 EMPLOYER"
-        ]
-
-
-@pytest.mark.parametrize(
-    "busy_for, expected", [
-        (1, 0),
-        (0, 0),
-        (2, 1)
+        "0 EMPLOYER",
     ]
-)
+
+
+@pytest.mark.parametrize("busy_for, expected", [(1, 0), (0, 0), (2, 1)])
 def test_init_day(busy_for, expected):
     state: State = State("test")
     employee: Optional[Employee]
@@ -40,6 +35,30 @@ def test_init_day(busy_for, expected):
         employee.busy_for = busy_for
         init_day(state)
         assert employee.busy_for == expected
+
+
+def test_manager_sell():
+    state: State = State("test")
+
+    state._my_farm.add_field()
+    state._my_farm.add_field()
+    state._my_farm.fields[1].content = Vegetable.PATATE
+    state._my_farm.fields[1]._is_sellable = True
+    state.sell()
+    assert state._is_busy == 3
+
+
+@pytest.mark.parametrize("day, expected", [(1, 2), (2, 1), (3, 0)])
+def test_init_day_sell(day, expected):
+    state: State = State("test")
+
+    state._my_farm.add_field()
+    state._my_farm.fields[0].content = Vegetable.PATATE
+    state._my_farm.fields[0]._is_sellable = True
+    state.sell()
+    for _ in range(day):
+        init_day(state)
+    assert state._is_busy == expected
 
 
 def test_give_job_employee():
