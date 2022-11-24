9a504468f19468e4a66e6c8b87ed92012f7415eb
JosephEAK <josephakoubia@gmail.com>
2022-11-24 11:03:01 +0100

    fonction sell

--- player/player_logic/state.py
+++ player/player_logic/state.py
@@ -1,14 +1,28 @@
 from dataclasses import dataclass
 from typing import List
 from .farm import Farm
+from .field import Field
+from typing import List, Optional
 
 
 @dataclass
-class State():
+class State:
     _my_farm: Farm
     _username: str
     _day: int = 0
-    _is_busy: bool = False
+    _is_busy: int = 0  # if sell == 2
+    # create function sell, with parameter the field, return the command exemple: 0 sell 1
+    # call her in_next_action if only the field can be sell
+
+    @property
+    def is_busy(self):
+        return self._is_busy
+
+    @is_busy.setter
+    def is_busy(self, busy: int) -> None:
+        self._is_busy = busy
+        if self._is_busy < 0:
+            self._is_busy = 0
 
     def __init__(self, username: str) -> None:
         self._username = username
@@ -19,6 +33,13 @@ class State():
         for farm in data["farms"]:
             if farm["name"] == self._username:
                 self._my_farm.read_data(farm)
+
+    def sell(self) -> str:
+        field = self._my_farm.sellable_field()
+        if field != None and self._is_busy == 0:
+            self._is_busy = 3
+            return f"0 VENDRE {field.location}"
+        return ""
 
     @property
     def my_farm(self) -> Farm:
