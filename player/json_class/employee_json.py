e8b9556e1a9963edf21171f5ba7748412612b052
matt2905 <matthias2905@hotmail.fr>
2022-11-24 16:24:21 +0100

    tractor

--- player/json_class/employee_json.py
+++ player/json_class/employee_json.py
@@ -1,9 +1,11 @@
 from pydantic import BaseModel
 from typing import Optional
 
+from player.json_class.tractor_json import TractorJSON
+
 
 class EmployeeJSON(BaseModel):
     id: int
     location: str
     salary: int
-    tractor: Optional[int]
+    tractor: Optional[TractorJSON]
